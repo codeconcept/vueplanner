01bb2a448d7117e9105c77f89f4186ec19ea5cba
codeconcept <contact@codeconcept.fr>
2018-01-22 12:12:01 +0100

    creating a filter using date-fns that formats dates

--- src/components/Techno.vue
+++ src/components/Techno.vue
@@ -2,14 +2,23 @@
     <div class="techno-details">
         <h3>{{techno.name}}</h3>
         <div>{{techno.details}}</div>
+        <div><small>ajoutée le : {{techno.dateCreation | dateAndTime}}</small></div>
+        <div><small>à faire le : {{techno.dateStudy | dateAndTime}}</small></div>
     </div>
 </template>
 
 <script>
+import format from 'date-fns/format';
+
 export default {
-  name: 'Techno',
-  props: ['techno']
-}
+  name: "Techno",
+  props: ["techno"],
+  filters: {
+    dateAndTime(value) {
+      return format(value, "DD/MM/YYYY à HH:mm");
+    }
+  }
+};
 </script>
 
 <style>
